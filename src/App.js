bc6fb81476ffc8098e7dab66320f87fced267068
PasupuletiRaja <98725681+PasupuletiRaja@users.noreply.github.com>
2023-11-18 18:39:19 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Inputfield from "./Components/Inputfield"
+
+function App() {
+  return (
+    <div className="App">
+      <Inputfield />
+    </div>
+  );
+}
+
+export default App;
